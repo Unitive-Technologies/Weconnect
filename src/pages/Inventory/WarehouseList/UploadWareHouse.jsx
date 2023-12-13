23b1d16f602c0d23dc736740331d52d4357ba4a9
FatimaMary <111224454+FatimaMary@users.noreply.github.com>
2023-12-13 19:11:46 +0530

    Update warehouselist.jsx

---
+++ src/pages/Inventory/WarehouseList/UploadWareHouse.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function UploadWareHouse() {
+  return <div>UploadWareHouse</div>;
+}
+
+export default UploadWareHouse;
---
+++ src/pages/Inventory/WarehouseList/ViewWareHouse.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function ViewWareHouse() {
+  return <div>ViewWareHouse</div>;
+}
+
+export default ViewWareHouse;
