df4e6736eec8ef04d64fd76c20d3e0a3df1fac11
stella-mary <mariastellamary@outlook.com>
2023-12-30 14:54:53 +0530

    Update actions.js

--- src/store/schedulecustomernotification/actions.js
+++ src/store/schedulecustomernotification/actions.js
@@ -2,6 +2,21 @@ import {
   GET_SCHEDULECUSTOMERNOTIFICATION,
   GET_SCHEDULECUSTOMERNOTIFICATION_FAIL,
   GET_SCHEDULECUSTOMERNOTIFICATION_SUCCESS,
+  GET_SCHEDULECUSTOMERNOTIFICATION_TYPE,
+  GET_SCHEDULECUSTOMERNOTIFICATION_TYPE_FAIL,
+  GET_SCHEDULECUSTOMERNOTIFICATION_TYPE_SUCCESS,
+  GET_SCHEDULECUSTOMERNOTIFICATION_OSD,
+  GET_SCHEDULECUSTOMERNOTIFICATION_OSD_FAIL,
+  GET_SCHEDULECUSTOMERNOTIFICATION_OSD_SUCCESS,
+  GET_SCHEDULECUSTOMERNOTIFICATION_BMAIL,
+  GET_SCHEDULECUSTOMERNOTIFICATION_BMAIL_FAIL,
+  GET_SCHEDULECUSTOMERNOTIFICATION_BMAIL_SUCCESS,
+  GET_SCHEDULECUSTOMERNOTIFICATION_SMS,
+  GET_SCHEDULECUSTOMERNOTIFICATION_SMS_FAIL,
+  GET_SCHEDULECUSTOMERNOTIFICATION_SMS_SUCCESS,
+  GET_SCHEDULECUSTOMERNOTIFICATION_STATUS,
+  GET_SCHEDULECUSTOMERNOTIFICATION_STATUS_FAIL,
+  GET_SCHEDULECUSTOMERNOTIFICATION_STATUS_SUCCESS,
   ADD_NEW_SCHEDULECUSTOMERNOTIFICATION,
   ADD_SCHEDULECUSTOMERNOTIFICATION_SUCCESS,
   ADD_SCHEDULECUSTOMERNOTIFICATION_FAIL,
@@ -42,5 +57,91 @@ export const addScheduleCustomerNotificationSuccess = (
 
 export const addScheduleCustomerNotificationFail = (error) => ({
   type: ADD_SCHEDULECUSTOMERNOTIFICATION_FAIL,
+  payload: error,
+});
+
+export const getScheduleCustomerNotificationType = () => ({
+  type: GET_SCHEDULECUSTOMERNOTIFICATION_TYPE,
+});
+
+export const getScheduleCustomerNotificationTypeSuccess = (designationType) => {
+  // console.log("Received Designation:", designation);
+  return {
+    type: GET_SCHEDULECUSTOMERNOTIFICATION_TYPE_SUCCESS,
+    payload: SchCusNotType,
+  };
+};
+
+export const getScheduleCustomerNotificationTypeFail = (error) => ({
+  type: GET_SCHEDULECUSTOMERNOTIFICATION_TYPE_FAIL,
+  payload: error,
+});
+
+export const getScheduleCustomerNotificationOSD = () => ({
+  type: GET_SCHEDULECUSTOMERNOTIFICATION_OSD,
+});
+
+export const getScheduleCustomerNotificationOSDSuccess = (designationType) => {
+  // console.log("Received Designation:", designation);
+  return {
+    type: GET_SCHEDULECUSTOMERNOTIFICATION_OSD_SUCCESS,
+    payload: SchCusNotOSD,
+  };
+};
+
+export const getScheduleCustomerNotificationOSDFail = (error) => ({
+  type: GET_SCHEDULECUSTOMERNOTIFICATION_OSD_FAIL,
+  payload: error,
+});
+
+
+export const getScheduleCustomerNotificationBmail = () => ({
+  type: GET_SCHEDULECUSTOMERNOTIFICATION_BMAIL,
+});
+
+export const getScheduleCustomerNotificationBmailSuccess = (designationType) => {
+  // console.log("Received Designation:", designation);
+  return {
+    type: GET_SCHEDULECUSTOMERNOTIFICATION_BMAIL_SUCCESS,
+    payload: SchCusNotBmail,
+  };
+};
+
+export const getScheduleCustomerNotificationBmailFail = (error) => ({
+  type: GET_SCHEDULECUSTOMERNOTIFICATION_BMAIL_FAIL,
+  payload: error,
+});
+
+export const getScheduleCustomerNotificationSMS = () => ({
+  type: GET_SCHEDULECUSTOMERNOTIFICATION_SMS,
+});
+
+export const getScheduleCustomerNotificationSMSSuccess = (designationType) => {
+  // console.log("Received Designation:", designation);
+  return {
+    type: GET_SCHEDULECUSTOMERNOTIFICATION_SMS_SUCCESS,
+    payload: SchCusNotSMS,
+  };
+};
+
+export const getScheduleCustomerNotificationSMSFail = (error) => ({
+  type: GET_SCHEDULECUSTOMERNOTIFICATION_SMS_FAIL,
+  payload: error,
+});
+
+export const getScheduleCustomerNotificationStatus = () => ({
+  type: GET_SCHEDULECUSTOMERNOTIFICATION_STATUS,
+});
+
+export const getScheduleCustomerNotificationStatusSuccess = (designationType) => {
+  // console.log("Received Designation:", designation);
+  return {
+    type: GET_SCHEDULECUSTOMERNOTIFICATION_STATUS_SUCCESS,
+    payload: SchCusNotStatus,
+  };
+};
+
+export const getScheduleCustomerNotificationStatusFail = (error) => ({
+  type: GET_SCHEDULECUSTOMERNOTIFICATION_STATUS_FAIL,
   payload: error,
 });
