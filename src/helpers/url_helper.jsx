f7a093bd44a5a427292994abbcbaed4a041ca051
stella-mary <mariastellamary@outlook.com>
2024-01-08 14:26:16 +0530

    Update url_helper.jsx

--- src/helpers/url_helper.jsx
+++ src/helpers/url_helper.jsx
@@ -181,7 +181,7 @@ export const GET_NCF =
   "/ncf-rates?expand=status_lbl,created_by_lbl,type_lbl&page=1&per-page=50&vr=web1.0";
 export const ADD_NCF = "/ncf-rates?vr=web1.0";
 export const GET_OPERATOR_FORBULKASSIGN =
-  "/operator/list?fields=id,name,code&expand=type_lbl,status_lbl,branch_lbl,distributor_lbl&notfilter[type]=0&filter[type]=3&notfilter[ncf_id]=3&page=1&per-page=500&vr=web1.0";
+  "/9operator/list?fields=id,name,code&expand=type_lbl,status_lbl,branch_lbl,distributor_lbl&notfilter[type]=0&filter[type]=3&notfilter[ncf_id]=3&page=1&per-page=500&vr=web1.0";
 export const ADD_BULKASSIGN_NCF = "/ncf-rates/assign?vr=web1.0";
 
 export const GET_COMPANYLIST = "/companylist";
