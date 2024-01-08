49bd15bad0ebec76dd78fa082dd66853eabd41e1
FatimaMary <111224454+FatimaMary@users.noreply.github.com>
2024-01-08 18:12:50 +0530

    Update reducer.js

--- src/store/bouquetlist/reducer.js
+++ src/store/bouquetlist/reducer.js
@@ -3,10 +3,31 @@ import {
   GET_BOUQUET_FAIL,
   ADD_BOUQUET_SUCCESS,
   ADD_BOUQUET_FAIL,
+  GET_BOUQUETTYPE_SUCCESS,
+  GET_BOUQUETTYPE_FAIL,
+  GET_BOUQUET_BOXTYPE_SUCCESS,
+  GET_BOUQUET_BOXTYPE_FAIL,
+  GET_BOUQUEX_SUCCESS,
+  GET_BOUQUEX_FAIL,
+  GET_BOUQUET_TAXLIST_SUCCESS,
+  GET_BOUQUET_TAXLIST_FAIL,
+  GET_RECHARGEPERIOD_SUCCESS,
+  GET_RECHARGEPERIOD_FAIL,
+  GET_ALACARTECHANNALS_SUCCESS,
+  GET_ALACARTECHANNALS_FAIL,
+  GET_BOUQUET_PACKAGES_SUCCESS,
+  GET_BOUQUET_PACKAGES_FAIL,
 } from "./actionTypes";
 
 const INIT_STATE = {
   bouquet: [],
+  bouquettype: [],
+  bouquetboxtype: [],
+  bouquex: [],
+  bouquettaxlist: [],
+  rechargeperiod: [],
+  alacartechannals: [],
+  bouquetpackages: [],
   error: {},
   loading: true,
 };
@@ -34,6 +55,97 @@ const Bouquet = (state = INIT_STATE, action) => {
       };
 
     case ADD_BOUQUET_FAIL:
+      return {
+        ...state,
+        error: action.payload,
+      };
+
+    case GET_BOUQUETTYPE_SUCCESS:
+      return {
+        ...state,
+        bouquettype: action.payload,
+        loading: false,
+      };
+
+    case GET_BOUQUETTYPE_FAIL:
+      return {
+        ...state,
+        error: action.payload,
+      };
+
+    case GET_BOUQUET_BOXTYPE_SUCCESS:
+      return {
+        ...state,
+        bouquetboxtype: action.payload,
+        loading: false,
+      };
+
+    case GET_BOUQUET_BOXTYPE_FAIL:
+      return {
+        ...state,
+        error: action.payload,
+      };
+
+    case GET_BOUQUEX_SUCCESS:
+      return {
+        ...state,
+        bouquex: action.payload,
+        loading: false,
+      };
+
+    case GET_BOUQUEX_FAIL:
+      return {
+        ...state,
+        error: action.payload,
+      };
+
+    case GET_BOUQUET_TAXLIST_SUCCESS:
+      return {
+        ...state,
+        bouquettaxlist: action.payload,
+        loading: false,
+      };
+
+    case GET_BOUQUET_TAXLIST_FAIL:
+      return {
+        ...state,
+        error: action.payload,
+      };
+
+    case GET_RECHARGEPERIOD_SUCCESS:
+      return {
+        ...state,
+        rechargeperiod: action.payload,
+        loading: false,
+      };
+
+    case GET_RECHARGEPERIOD_FAIL:
+      return {
+        ...state,
+        error: action.payload,
+      };
+
+    case GET_ALACARTECHANNALS_SUCCESS:
+      return {
+        ...state,
+        alacartechannals: action.payload,
+        loading: false,
+      };
+
+    case GET_ALACARTECHANNALS_FAIL:
+      return {
+        ...state,
+        error: action.payload,
+      };
+
+    case GET_BOUQUET_PACKAGES_SUCCESS:
+      return {
+        ...state,
+        bouquetpackages: action.payload,
+        loading: false,
+      };
+
+    case GET_BOUQUET_PACKAGES_FAIL:
       return {
         ...state,
         error: action.payload,
