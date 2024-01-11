5b8c9179b4127011d658f9c21f17cbc44cc2b06d
FatimaMary <111224454+FatimaMary@users.noreply.github.com>
2024-01-11 16:47:54 +0530

    Update BulkAssigntoOperator.jsx

--- src/pages/Subscription/NCFList/BulkAssigntoOperator.jsx
+++ src/pages/Subscription/NCFList/BulkAssigntoOperator.jsx
@@ -24,67 +24,6 @@ const BulkAssigntoOperator = (props) => {
   const { isOpen, toggle, ncf, selectedRow } = props;
   const dispatch = useDispatch();
 
-  // console.log("Selected row: ", selectedRow);
-
-  const validation = useFormik({
-    // enableReinitialize : use this flag when initial values needs to be changed
-    enableReinitialize: true,
-
-    initialValues: {
-      name: (ncf && ncf.name) || "",
-      code: (ncf && ncf.code) || "",
-      status: (ncf && ncf.status) || "",
-      calculate_per_channel: (ncf && ncf.calculate_per_channel) || "",
-      from_channel_no: (ncf && ncf.from_channel_no) || "",
-      to_channel_no: (ncf && ncf.to_channel_no) || "",
-      is_refundable: (ncf && ncf.is_refundable) || "",
-      mrp: (ncf && ncf.mrp) || "",
-      lmo_discount: (ncf && ncf.lmo_discount) || "",
-      lmo_rate: (ncf && ncf.lmo_rate) || "",
-      created_at: (ncf && ncf.created_at) || "",
-      created_by: (ncf && ncf.created_by) || "my mso(mso)",
-      status_lbl: (ncf && ncf.status_lbl) || "",
-    },
-    validationSchema: Yup.object({
-      name: Yup.string().required("Enter name"),
-      code: Yup.string().required("Enter code"),
-      status: Yup.string().required("Select status"),
-      calculate_per_channel: Yup.string().required(
-        "Select calculate per channel"
-      ),
-      from_channel_no: Yup.string().required("Enter from channel"),
-      to_channel_no: Yup.string().required("Enter to channel"),
-      is_refundable: Yup.string().required("Select refundable"),
-      mrp: Yup.string(),
-      lmo_discount: Yup.string(),
-      lmo_rate: Yup.string(),
-    }),
-    onSubmit: (values) => {
-      const updateNcf = {
-        id: Math.floor(Math.random() * (30 - 20)) + 20,
-        name: values["name"],
-        code: values["code"],
-        status: values["status"],
-        calculate_per_channel: values["calculate_per_channel"],
-        from_channel_no: values["from_channel_no"],
-        to_channel_no: values["to_channel_no"],
-        is_refundable: values["is_refundable"],
-        mrp: values["mrp"],
-        lmo_discount: values["lmo_discount"],
-        lmo_rate: values["lmo_rate"],
-        created_at: new Date(),
-        created_by: values["created_by"],
-      };
-      console.log("Update NCF:" + JSON.stringify(updateNcf));
-      dispatch(onAddNcf(updateNcf));
-      validation.resetForm();
-      toggle();
-    },
-    onReset: (values) => {
-      validation.setValues(validation.initialValues);
-    },
-  });
-
   return (
     <Modal
       isOpen={isOpen}
