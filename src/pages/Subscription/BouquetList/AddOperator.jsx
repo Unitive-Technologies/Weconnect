c167ce2436b3c858dd21b87b1804246312e3f953
FatimaMary <111224454+FatimaMary@users.noreply.github.com>
2024-01-12 18:44:11 +0530

    Update AddOperator.jsx

--- src/pages/Subscription/BouquetList/AddOperator.jsx
+++ src/pages/Subscription/BouquetList/AddOperator.jsx
@@ -17,7 +17,28 @@ import { createSelector } from "reselect";
 
 const AddOperators = (props) => {
   const { isOpen, toggle, id } = props;
-  // console.log("Selected row id: ", id);
+  const [selectedUsers, setSelectedUsers] = useState([]);
+
+  const handleActive = (row) => {
+    const isRowSelected = selectedUsers.some((user) => user.id === row.id);
+
+    setTableList((prevTableList) =>
+      prevTableList.filter((user) => user.id !== row.id)
+    );
+
+    if (isRowSelected) {
+      setSelectedUsers((prevSelectedUsers) =>
+        prevSelectedUsers.filter((user) => user.id !== row.id)
+      );
+    } else {
+      setSelectedUsers((prevSelectedUsers) => [...prevSelectedUsers, row]);
+    }
+
+    // Ensure that row.original exists before accessing its properties
+    if (row.original) {
+      row.original.isSelected = !isRowSelected;
+    }
+  };
 
   const dispatch = useDispatch();
 
@@ -37,6 +58,22 @@ const AddOperators = (props) => {
       dispatch(onGetOperatorForBouquet(id));
     }
   }, [dispatch, operatorforbulkbouquet, id]);
+
+  const handleRemove = (row) => {
+    if (selectedUsers) {
+      setSelectedUsers((prevSelectedUsers) =>
+        prevSelectedUsers.filter((user) => user.id !== row.id)
+      );
+      setTableList((prevTableList) =>
+        prevTableList.map((user) => {
+          if (user.id === row.id && user.original) {
+            user.original.isSelected = false;
+          }
+          return user;
+        })
+      );
+    }
+  };
 
   const columns = useMemo(
     () => [
@@ -127,6 +164,106 @@ const AddOperators = (props) => {
     []
   );
 
+  const userColumns = useMemo(
+    () => [
+      //   {
+      //     Header: "*",
+      //     disableFilters: true,
+      //     id: "*",
+      //     filterable: true,
+      //     Cell: (cellProps) => {
+      //       return <input type="checkbox" />;
+      //     },
+      //   },
+      {
+        Header: "#",
+        disableFilters: true,
+        filterable: true,
+        Cell: (cellProps) => {
+          const totalRows = cellProps.rows.length;
+          const reverseIndex = totalRows - cellProps.row.index;
+
+          return (
+            <>
+              <h5 className="font-size-14 mb-1">
+                <Link className="text-dark" to="#">
+                  {reverseIndex}
+                </Link>
+              </h5>
+            </>
+          );
+        },
+      },
+
+      {
+        Header: "Name",
+        accessor: "name",
+        filterable: true,
+        Cell: (cellProps) => {
+          return (
+            <p className="text-muted mb-0">{cellProps.row.original.name}</p>
+          );
+        },
+      },
+      {
+        Header: "Code",
+        accessor: "code",
+        filterable: true,
+        Cell: (cellProps) => {
+          return (
+            <p className="text-muted mb-0">{cellProps.row.original.code}</p>
+          );
+        },
+      },
+      {
+        Header: "Type",
+        accessor: "type_lbl",
+        filterable: true,
+        Cell: (cellProps) => {
+          return (
+            <p className="text-muted mb-0">{cellProps.row.original.type_lbl}</p>
+          );
+        },
+      },
+      {
+        Header: "Reginal Officer",
+        accessor: "branch_lbl",
+        filterable: true,
+        Cell: (cellProps) => {
+          return (
+            <p className="text-muted mb-0">
+              {cellProps.row.original.branch_lbl}
+            </p>
+          );
+        },
+      },
+      {
+        Header: "Distributor",
+        accessor: "distributor_lbl",
+        filterable: true,
+        Cell: (cellProps) => {
+          return (
+            <p className="text-muted mb-0">
+              {cellProps.row.original.distributor_lbl}
+            </p>
+          );
+        },
+      },
+      {
+        Header: "..",
+        Cell: (cellProps) => {
+          return (
+            <i
+              className="dripicons-tag-delete"
+              onClick={() => handleRemove(cellProps.row.original)}
+            />
+          );
+        },
+      },
+    ],
+    []
+  );
+
   return (
     <Modal
       isOpen={isOpen}
@@ -150,6 +287,9 @@ const AddOperators = (props) => {
                 columns={columns}
                 data={operatorforbulkbouquet}
                 isGlobalFilter={true}
+                handleRowClick={(row) => {
+                  handleActive(row);
+                }}
                 isShowingPageLength={true}
                 tableClass="table align-middle table-nowrap table-hover"
                 theadClass="table-light"
@@ -159,6 +299,45 @@ const AddOperators = (props) => {
             )}
           </CardBody>
         </Card>
+        <div
+          style={{
+            // margin: "20px 0px",
+            marginTop: "20px",
+            marginBottom: "-18px",
+            zIndex: 12000,
+            backgroundColor: "#fff",
+            width: "fit-content",
+            marginLeft: "40%",
+            position: "absolute",
+            padding: "0px 10px",
+          }}
+        >
+          {" "}
+          <h5 style={{}}>Selected Operators</h5>
+        </div>
+        <Row
+          style={{
+            position: "relative",
+            border: "1px solid #ced4da",
+            padding: "20px 0px",
+            margin: "30px 0px",
+          }}
+        >
+          <Col lg={12}>
+            <TableContainer
+              isPagination={true}
+              columns={userColumns}
+              data={selectedUsers}
+              //   isGlobalFilter={true}
+              isShowingPageLength={true}
+              customPageSize={50}
+              tableClass="table align-middle table-nowrap table-hover"
+              theadClass="table-light"
+              paginationDiv="col-sm-12 col-md-7"
+              pagination="pagination pagination-rounded justify-content-end mt-4"
+            />
+          </Col>
+        </Row>
         <div className="text-center mt-4 ">
           <div
             style={{
