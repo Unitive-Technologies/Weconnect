2bf95e13a9e3105501fa77ce40b7e6922157f309
FatimaMary <111224454+FatimaMary@users.noreply.github.com>
2024-01-31 12:05:35 +0530

    Update CreatePairing.jsx

--- src/pages/InventoryStock/CreatePairing.jsx
+++ src/pages/InventoryStock/CreatePairing.jsx
@@ -1,5 +1,7 @@
-import React from "react";
+import React, { useMemo } from "react";
 import {
+  Card,
+  CardBody,
   Col,
   Form,
   Input,
@@ -10,9 +12,150 @@ import {
   Row,
 } from "reactstrap";
 import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
+import TableContainer from "../../components/Common/TableContainer";
 
 function CreatePairing(props) {
   const { isOpen, toggle, smartcardlist, stblist, stocksccastype } = props;
+
+  const smartcardColumns = useMemo(
+    () => [
+      {
+        Header: ".",
+        disableFilters: true,
+        filterable: true,
+        Cell: (cellProps) => (
+          <>
+            <input type="checkbox" />
+          </>
+        ),
+      },
+      {
+        Header: "#",
+        disableFilters: true,
+        filterable: true,
+        Cell: (cellProps) => {
+          const totalRows = cellProps.rows.length;
+          const reverseIndex = totalRows - cellProps.row.index;
+
+          return (
+            <>
+              <h5 className="font-size-14 mb-1">
+                <Link className="text-dark" to="#">
+                  {reverseIndex}
+                </Link>
+              </h5>
+            </>
+          );
+        },
+      },
+      {
+        Header: "Smartcard No.",
+        accessor: "smartcardno",
+        filterable: true,
+        Cell: (cellProps) => {
+          return (
+            <>
+              <h5 className="font-size-14 mb-1">
+                <Link className="text-dark" to="#">
+                  {cellProps.row.original.smartcardno}
+                </Link>
+              </h5>
+            </>
+          );
+        },
+      },
+      {
+        Header: "Brand",
+        accessor: "brand_lbl",
+        filterable: true,
+        Cell: (cellProps) => {
+          return (
+            <p className="text-muted mb-0">
+              {cellProps.row.original.brand_lbl}
+            </p>
+          );
+        },
+      },
+    ],
+    []
+  );
+
+  const stbColumns = useMemo(
+    () => [
+      {
+        Header: ".",
+        disableFilters: true,
+        filterable: true,
+        Cell: (cellProps) => (
+          <>
+            <input type="checkbox" />
+          </>
+        ),
+      },
+      {
+        Header: "#",
+        disableFilters: true,
+        filterable: true,
+        Cell: (cellProps) => {
+          const totalRows = cellProps.rows.length;
+          const reverseIndex = totalRows - cellProps.row.index;
+
+          return (
+            <>
+              <h5 className="font-size-14 mb-1">
+                <Link className="text-dark" to="#">
+                  {reverseIndex}
+                </Link>
+              </h5>
+            </>
+          );
+        },
+      },
+      {
+        Header: "STB No.",
+        accessor: "stbno",
+        filterable: true,
+        Cell: (cellProps) => {
+          return (
+            <>
+              <h5 className="font-size-14 mb-1">
+                <Link className="text-dark" to="#">
+                  {cellProps.row.original.stbno}
+                </Link>
+              </h5>
+            </>
+          );
+        },
+      },
+      {
+        Header: "Brand",
+        accessor: "brand_lbl",
+        filterable: true,
+        Cell: (cellProps) => {
+          return (
+            <p className="text-muted mb-0">
+              {cellProps.row.original.brand_lbl}
+            </p>
+          );
+        },
+      },
+      {
+        Header: "Box Type",
+        accessor: "boxtype_lbl",
+        filterable: true,
+        Cell: (cellProps) => {
+          return (
+            <p className="text-muted mb-0">
+              {cellProps.row.original.boxtype_lbl}
+            </p>
+          );
+        },
+      },
+    ],
+    []
+  );
+
   return (
     <Modal
       isOpen={isOpen}
@@ -61,6 +204,42 @@ function CreatePairing(props) {
                   </FormFeedback>
                 ) : null} */}
               </div>
+            </Col>
+          </Row>
+          <Row>
+            <Col lg={6}>
+              <Card>
+                <CardBody>
+                  <TableContainer
+                    isPagination={true}
+                    columns={smartcardColumns}
+                    data={smartcardlist}
+                    isShowingPageLength={true}
+                    customPageSize={50}
+                    tableClass="table align-middle table-nowrap table-hover"
+                    theadClass="table-light"
+                    paginationDiv="col-sm-12 col-md-7"
+                    pagination="pagination pagination-rounded justify-content-end mt-4"
+                  />
+                </CardBody>
+              </Card>
+            </Col>
+            <Col lg={6}>
+              <Card>
+                <CardBody>
+                  <TableContainer
+                    isPagination={true}
+                    columns={stbColumns}
+                    data={stblist}
+                    isShowingPageLength={true}
+                    customPageSize={50}
+                    tableClass="table align-middle table-nowrap table-hover"
+                    theadClass="table-light"
+                    paginationDiv="col-sm-12 col-md-7"
+                    pagination="pagination pagination-rounded justify-content-end mt-4"
+                  />
+                </CardBody>
+              </Card>
             </Col>
           </Row>
         </Form>
