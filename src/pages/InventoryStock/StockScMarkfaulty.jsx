f15cd2eeeb6daa5f4b532b79faaa4dcbe270afcd
FatimaMary <111224454+FatimaMary@users.noreply.github.com>
2024-02-03 17:40:21 +0530

    Update StockScMarkfaulty.jsx

--- src/pages/InventoryStock/StockScMarkfaulty.jsx
+++ src/pages/InventoryStock/StockScMarkfaulty.jsx
@@ -1,9 +1,60 @@
-import React from "react";
-import { Modal, ModalBody, ModalHeader } from "reactstrap";
+import React, { useMemo } from "react";
+import {
+  Card,
+  CardBody,
+  Col,
+  Modal,
+  ModalBody,
+  ModalHeader,
+  Row,
+} from "reactstrap";
 import PropTypes from "prop-types";
+import TableContainer from "../../components/Common/TableContainer";
 
 function StockScMarkfaulty(props) {
   const { isOpen, toggle } = props;
+
+  const columns = useMemo(
+    () => [
+      {
+        Header: ".",
+        disableFilters: true,
+        filterable: true,
+        Cell: () => {
+          <input type="checkbox" />;
+        },
+      },
+      {
+        Header: "#",
+        disableFilters: true,
+        filterable: true,
+        Cell: (cellProps) => {
+          const startIndex = (currentPage - 1) * pageSize;
+          const index = startIndex + cellProps.row.index + 1;
+          return (
+            <>
+              <h5 className="font-size-14 mb-1">{index}</h5>
+            </>
+          );
+        },
+      },
+      {
+        Header: "Smartcard No.",
+        accessor: "smartcardno",
+        filterable: true,
+        Cell: (cellProps) => {
+          return (
+            <>
+              <h5 className="font-size-14 mb-1">
+                {cellProps.row.original.smartcardno}
+              </h5>
+            </>
+          );
+        },
+      },
+    ],
+    []
+  );
   return (
     <Modal
       isOpen={isOpen}
@@ -16,7 +67,27 @@ function StockScMarkfaulty(props) {
       toggle={toggle}
     >
       <ModalHeader toggle={toggle}>Mark Faulty Smartcard</ModalHeader>
-      <ModalBody></ModalBody>
+      <ModalBody>
+        <Row>
+          <Col lg="12">
+            <Card>
+              <CardBody>
+                <TableContainer
+                  isPagination={true}
+                  columns={columns}
+                  data={[]}
+                  isShowingPageLength={true}
+                  customPageSize={50}
+                  tableClass="table align-middle table-nowrap table-hover"
+                  theadClass="table-light"
+                  paginationDiv="col-sm-12 col-md-7"
+                  pagination="pagination pagination-rounded justify-content-end mt-4"
+                />
+              </CardBody>
+            </Card>
+          </Col>
+        </Row>
+      </ModalBody>
     </Modal>
   );
 }
