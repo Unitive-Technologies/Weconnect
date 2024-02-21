d8bccd7219f9f12da11d6e281d8f2119d31639df
stella-mary <mariastellamary@outlook.com>
2024-02-21 19:12:07 +0530

    Finished Territory,  Complaint =>  History

--- src/pages/Territory/CityList/ViewCity.jsx
+++ src/pages/Territory/CityList/ViewCity.jsx
@@ -21,12 +21,19 @@ import {
   getCity as onGetCity,
   getDistrictByStateid as onGetDistrictByStateid,
 } from "/src/store/actions";
+import ShowHistoryModal from "./ShowHistoryModal";
 
 const ViewCity = (props) => {
   const { isOpen, handleViewCity, city, status, statelist, districtlist } =
     props;
   const dispatch = useDispatch();
   const [showEditCity, setShowEditCity] = useState(false);
+
+  const [showHistory, setShowHistory] = useState(false);
+
+  const toggleHistoryModal = () => {
+    setShowHistory(!showHistory);
+  };
 
   const validation = useFormik({
     // enableReinitialize : use this flag when initial values needs to be changed
@@ -84,218 +91,241 @@ const ViewCity = (props) => {
   };
 
   return (
-    <Modal
-      isOpen={isOpen}
-      role="dialog"
-      size="xl"
-      autoFocus={true}
-      centered={true}
-      className="exampleModal"
-      tabIndex="-1"
-      toggle={handleCancel}
-    >
-      <ModalHeader toggle={handleCancel} tag="h4">
-        {!showEditCity
-          ? `View ${(city && city.name) || ""}`
-          : `Edit ${(city && city.name) || ""}`}
-      </ModalHeader>
-      {!showEditCity && (
-        <Link
-          style={{
-            position: "absolute",
-            marginLeft: "92%",
-            marginTop: "1%",
-          }}
-          to="#!"
-          className="btn btn-light me-1"
-          onClick={() => setShowEditCity(true)}
-        >
-          <i className="mdi mdi-pencil-outline"></i>
-        </Link>
+    <>
+      {showHistory && (
+        <ShowHistoryModal
+          isOpen={showHistory}
+          toggleHistoryModal={toggleHistoryModal}
+          city={city}
+        />
       )}
-      <ModalBody>
-        <Form
-          onSubmit={(e) => {
-            e.preventDefault();
-            validation.handleSubmit();
-            return false;
-          }}
-        >
-          <Row>
-            <Col lg={4}>
-              <div className="mb-3">
-                <Label className="form-label">
-                  City Name<span style={{ color: "red" }}>*</span>
-                </Label>
-                <Input
-                  name="name"
-                  type="text"
-                  placeholder="Enter city name"
-                  onChange={validation.handleChange}
-                  onBlur={validation.handleBlur}
-                  value={validation.values.name || ""}
-                  disabled={!showEditCity}
-                  invalid={
-                    validation.touched.name && validation.errors.name
-                      ? true
-                      : false
-                  }
-                />
-                {validation.touched.name && validation.errors.name ? (
-                  <FormFeedback type="invalid">
-                    {validation.errors.name}
-                  </FormFeedback>
-                ) : null}
-              </div>
-            </Col>
-            <Col lg={4}>
-              <div className="mb-3">
-                <Label className="form-label">
-                  State<span style={{ color: "red" }}>*</span>
-                </Label>
-                <Input
-                  name="state_id"
-                  type="select"
-                  placeholder="Select state"
-                  className="form-select"
-                  onChange={validation.handleChange}
-                  onBlur={validation.handleBlur}
-                  value={validation.values.state_id || ""}
-                  disabled={!showEditCity}
-                >
-                  {statelist.map((options) => (
-                    <option key={options.id} value={options.id}>
-                      {options.name}
-                    </option>
-                  ))}
-                </Input>
-                {validation.touched.state_id && validation.errors.state_id ? (
-                  <FormFeedback type="invalid">
-                    {validation.errors.state_id}
-                  </FormFeedback>
-                ) : null}
-              </div>
-            </Col>
-            <Col lg={4}>
-              <div className="mb-3">
-                <Label className="form-label">
-                  District<span style={{ color: "red" }}>*</span>
-                </Label>
-                <Input
-                  name="district_id"
-                  type="select"
-                  placeholder="Select state"
-                  className="form-select"
-                  onChange={validation.handleChange}
-                  onBlur={validation.handleBlur}
-                  value={validation.values.district_id || ""}
-                  disabled={!showEditCity}
-                >
-                  {districtlist.map((options) => (
-                    <option key={options.id} value={options.id}>
-                      {options.name}
-                    </option>
-                  ))}
-                </Input>
-                {validation.touched.district_id &&
-                validation.errors.district_id ? (
-                  <FormFeedback type="invalid">
-                    {validation.errors.district_id}
-                  </FormFeedback>
-                ) : null}
-              </div>
-            </Col>
-            <Col lg={4}>
-              <div className="mb-3">
-                <Label className="form-label">
-                  Description<span style={{ color: "red" }}>*</span>
-                </Label>
-                <Input
-                  name="description"
-                  type="textarea"
-                  placeholder="Enter description"
-                  rows="3"
-                  onChange={validation.handleChange}
-                  onBlur={validation.handleBlur}
-                  value={validation.values.description || ""}
-                  invalid={
-                    validation.touched.description &&
-                    validation.errors.description
-                      ? true
-                      : false
-                  }
-                  disabled={!showEditCity}
-                />
-                {validation.touched.description &&
-                validation.errors.description ? (
-                  <FormFeedback type="invalid">
-                    {validation.errors.description}
-                  </FormFeedback>
-                ) : null}
-              </div>
-            </Col>
-            <Col lg={4}>
-              <div className="mb-3">
-                <Label className="form-label">
-                  Status<span style={{ color: "red" }}>*</span>
-                </Label>
-                <Input
-                  name="status"
-                  type="select"
-                  placeholder="Select Status"
-                  className="form-select"
-                  onChange={validation.handleChange}
-                  onBlur={validation.handleBlur}
-                  value={validation.values.status || ""}
-                  disabled={!showEditCity}
-                >
-                  {status.map((options) => (
-                    <option key={options.id} value={options.id}>
-                      {options.name}
-                    </option>
-                  ))}
-                </Input>
-                {validation.touched.status && validation.errors.status ? (
-                  <FormFeedback type="invalid">
-                    {validation.errors.status}
-                  </FormFeedback>
-                ) : null}
-              </div>
-            </Col>
-          </Row>
-          {showEditCity && (
+      <Modal
+        isOpen={isOpen}
+        role="dialog"
+        size="xl"
+        autoFocus={true}
+        centered={true}
+        className="exampleModal"
+        tabIndex="-1"
+        toggle={handleCancel}
+      >
+        <ModalHeader toggle={handleCancel} tag="h4">
+          {!showEditCity
+            ? `View ${(city && city.name) || ""}`
+            : `Edit ${(city && city.name) || ""}`}
+        </ModalHeader>
+        {!showEditCity && (
+          <>
+            <Link
+              style={{
+                position: "absolute",
+                marginLeft: "92%",
+                marginTop: "1%",
+              }}
+              to="#!"
+              className="btn btn-light me-1"
+              onClick={() => setShowHistory(true)}
+            >
+              <i className="dripicons-briefcase" />
+            </Link>
+            <Link
+              style={{
+                position: "absolute",
+                marginLeft: "87%",
+                marginTop: "1%",
+              }}
+              to="#!"
+              className="btn btn-light me-1"
+              onClick={() => setShowEditCity(true)}
+            >
+              <i className="mdi mdi-pencil-outline"></i>
+            </Link>
+          </>
+        )}
+        <ModalBody>
+          <Form
+            onSubmit={(e) => {
+              e.preventDefault();
+              validation.handleSubmit();
+              return false;
+            }}
+          >
             <Row>
-              <Col>
-                <ModalFooter>
-                  <button type="submit" className="btn btn-success save-user">
-                    Save
-                  </button>
-                  <button
-                    type="reset"
-                    className="btn btn-warning"
-                    onClick={() => validation.resetForm()}
+              <Col lg={4}>
+                <div className="mb-3">
+                  <Label className="form-label">
+                    City Name<span style={{ color: "red" }}>*</span>
+                  </Label>
+                  <Input
+                    name="name"
+                    type="text"
+                    placeholder="Enter city name"
+                    onChange={validation.handleChange}
+                    onBlur={validation.handleBlur}
+                    value={validation.values.name || ""}
+                    disabled={!showEditCity}
+                    invalid={
+                      validation.touched.name && validation.errors.name
+                        ? true
+                        : false
+                    }
+                  />
+                  {validation.touched.name && validation.errors.name ? (
+                    <FormFeedback type="invalid">
+                      {validation.errors.name}
+                    </FormFeedback>
+                  ) : null}
+                </div>
+              </Col>
+              <Col lg={4}>
+                <div className="mb-3">
+                  <Label className="form-label">
+                    State<span style={{ color: "red" }}>*</span>
+                  </Label>
+                  <Input
+                    name="state_id"
+                    type="select"
+                    placeholder="Select state"
+                    className="form-select"
+                    onChange={validation.handleChange}
+                    onBlur={validation.handleBlur}
+                    value={validation.values.state_id || ""}
+                    disabled={!showEditCity}
                   >
-                    Reset
-                  </button>
-
-                  <button
-                    type="button"
-                    className="btn btn-outline-danger"
-                    onClick={() => {
-                      validation.resetForm();
-                      handleCancel();
-                    }}
+                    {statelist.map((options) => (
+                      <option key={options.id} value={options.id}>
+                        {options.name}
+                      </option>
+                    ))}
+                  </Input>
+                  {validation.touched.state_id && validation.errors.state_id ? (
+                    <FormFeedback type="invalid">
+                      {validation.errors.state_id}
+                    </FormFeedback>
+                  ) : null}
+                </div>
+              </Col>
+              <Col lg={4}>
+                <div className="mb-3">
+                  <Label className="form-label">
+                    District<span style={{ color: "red" }}>*</span>
+                  </Label>
+                  <Input
+                    name="district_id"
+                    type="select"
+                    placeholder="Select state"
+                    className="form-select"
+                    onChange={validation.handleChange}
+                    onBlur={validation.handleBlur}
+                    value={validation.values.district_id || ""}
+                    disabled={!showEditCity}
                   >
-                    Cancel
-                  </button>
-                </ModalFooter>
+                    {districtlist.map((options) => (
+                      <option key={options.id} value={options.id}>
+                        {options.name}
+                      </option>
+                    ))}
+                  </Input>
+                  {validation.touched.district_id &&
+                    validation.errors.district_id ? (
+                    <FormFeedback type="invalid">
+                      {validation.errors.district_id}
+                    </FormFeedback>
+                  ) : null}
+                </div>
+              </Col>
+              <Col lg={4}>
+                <div className="mb-3">
+                  <Label className="form-label">
+                    Description<span style={{ color: "red" }}>*</span>
+                  </Label>
+                  <Input
+                    name="description"
+                    type="textarea"
+                    placeholder="Enter description"
+                    rows="3"
+                    onChange={validation.handleChange}
+                    onBlur={validation.handleBlur}
+                    value={validation.values.description || ""}
+                    invalid={
+                      validation.touched.description &&
+                        validation.errors.description
+                        ? true
+                        : false
+                    }
+                    disabled={!showEditCity}
+                  />
+                  {validation.touched.description &&
+                    validation.errors.description ? (
+                    <FormFeedback type="invalid">
+                      {validation.errors.description}
+                    </FormFeedback>
+                  ) : null}
+                </div>
+              </Col>
+              <Col lg={4}>
+                <div className="mb-3">
+                  <Label className="form-label">
+                    Status<span style={{ color: "red" }}>*</span>
+                  </Label>
+                  <Input
+                    name="status"
+                    type="select"
+                    placeholder="Select Status"
+                    className="form-select"
+                    onChange={validation.handleChange}
+                    onBlur={validation.handleBlur}
+                    value={validation.values.status || ""}
+                    disabled={!showEditCity}
+                  >
+                    {status.map((options) => (
+                      <option key={options.id} value={options.id}>
+                        {options.name}
+                      </option>
+                    ))}
+                  </Input>
+                  {validation.touched.status && validation.errors.status ? (
+                    <FormFeedback type="invalid">
+                      {validation.errors.status}
+                    </FormFeedback>
+                  ) : null}
+                </div>
               </Col>
             </Row>
-          )}
-        </Form>
-      </ModalBody>
-      {/* </Modal> */}
-    </Modal>
+            {showEditCity && (
+              <Row>
+                <Col>
+                  <ModalFooter>
+                    <button type="submit" className="btn btn-success save-user">
+                      Save
+                    </button>
+                    <button
+                      type="reset"
+                      className="btn btn-warning"
+                      onClick={() => validation.resetForm()}
+                    >
+                      Reset
+                    </button>
+
+                    <button
+                      type="button"
+                      className="btn btn-outline-danger"
+                      onClick={() => {
+                        validation.resetForm();
+                        handleCancel();
+                      }}
+                    >
+                      Cancel
+                    </button>
+                  </ModalFooter>
+                </Col>
+              </Row>
+            )}
+          </Form>
+        </ModalBody>
+        {/* </Modal> */}
+      </Modal>
+    </>
   );
 };
 
