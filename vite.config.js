95387ec5fa6bd91b3012182477953f440f7cc689
Anthoniselvi <anthoniselvi33@gmail.com>
2024-02-26 10:32:14 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,23 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   esbuild: {
+//     jsxFactory: 'h',
+//     jsxFragment: 'Fragment',
+//   }
+// })
+
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
 export default defineConfig({
   plugins: [react()],
-  esbuild: {
-    jsxFactory: 'h',
-    jsxFragment: 'Fragment',
-  }
-})
+  resolve: {
+    alias: {
+      "/src": "/src",
+    },
+  },
+});
