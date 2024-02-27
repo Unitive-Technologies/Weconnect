5e18e6592ade6b45c6ff7267c94395c43b686731
FatimaMary <111224454+FatimaMary@users.noreply.github.com>
2024-02-27 19:44:53 +0530

    Update UploadSmartcaed.jsx

--- src/pages/InventoryStock/UploadSmartcaed.jsx
+++ src/pages/InventoryStock/UploadSmartcaed.jsx
@@ -19,6 +19,12 @@ import Dropzone from "react-dropzone";
 import { useDispatch } from "react-redux";
 import { useFormik } from "formik";
 import * as Yup from "yup";
+import { addInventoryStockSmartcard as onAddInventoryStockSmartcard } from "/src/store/inventorystock/actions";
+import {
+  downloadSmartcardUploadTemplate,
+  updateSmartcardUploadByToken,
+  uploadSmartcardSubmit,
+} from "../../helpers/backend_helper";
 
 const UploadSmartcard = (props) => {
   const {
@@ -33,169 +39,117 @@ const UploadSmartcard = (props) => {
   } = props;
   const dispatch = useDispatch();
 
+  const [uploadTrigger, setUploadTrigger] = useState({});
   const [selectedFiles, setSelectedFiles] = useState([]);
+  const [status, setStatus] = useState("");
+  const [successMsg, setSuccessMsg] = useState(false);
+
+  const toggleSuccessMsg = () => {
+    setSuccessMsg(!successMsg);
+  };
 
   function handleAcceptedFiles(files) {
-    files.map((file) =>
-      Object.assign(file, {
-        preview: URL.createObjectURL(file),
-        formattedSize: formatBytes(file.size),
-      })
-    );
     setSelectedFiles(files);
+    updateSmartcardUploadByToken(
+      uploadTrigger.token,
+      smartcardBulkUpdateSavedTemplatePayload
+    )
+      .then((res) => {
+        console.log(
+          "res in updateSmartcardUploadByToken: " + JSON.stringify(res)
+        );
+      })
+      .catch((error) => {
+        console.log("error in updateSmartcardUploadByToken: " + error);
+      });
   }
 
-  function formatBytes(bytes, decimals = 2) {
-    if (bytes === 0) return "0 Bytes";
-    const k = 1024;
-    const dm = decimals < 0 ? 0 : decimals;
-    const sizes = ["Bytes", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"];
+  const smartcardBulkUpdateDownloadTemplatePayload = {
+    meta_data: { type: null },
+    url: "",
+  };
 
-    const i = Math.floor(Math.log(bytes) / Math.log(k));
-    return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + " " + sizes[i];
-  }
+  const smartcardBulkUpdateSavedTemplatePayload = {
+    meta_data: {
+      brand_id: 2,
+      cas_id: 1,
+      description: "Testing",
+      inv_state_id: 1,
+      invoice_date: "2024-02-27",
+      invoice_no: "aswwee",
+      po_date: "2024-02-26",
+      po_number: "aasdfe",
+      state: 1,
+      stb_brand: null,
+      stbbrand_id: null,
+      type: null,
+      warehouse_id: 4,
+    },
+    url: "",
+  };
 
   const handleDownloadSampleFile = () => {
-    // Create a sample CSV file with headers
-    // field to be get from api as prop
-    const headers = ["smartcardno", "other_id"];
-    const data = [headers];
+    // Send a POST request to the server, from the json request convert data.fields array of strings as headers in a csv file
+    downloadSmartcardUploadTemplate(smartcardBulkUpdateDownloadTemplatePayload)
+      .then((res) => {
+        debugger;
+        const fileName = res.data.data.type;
+        const fieldStringArray = res.data.data.fields;
+        //combine fieldStringArray contents into a single string seperated by commas
+        const headers = fieldStringArray.join(",");
+        // const csvContent = data.map((row) => row.join(",")).join("\n");
+        const blob = new Blob([headers], {
+          type: "text/csv;charset=utf-8;",
+        });
 
-    // Convert the data to CSV format
-    const csvContent = data.map((row) => row.join(",")).join("\n");
+        setUploadTrigger(res.data.data);
+        // Create a download link
+        const link = document.createElement("a");
+        link.href = window.URL.createObjectURL(blob);
+        link.download = fileName + ".csv";
 
-    // Create a Blob containing the data in CSV format
-    const blob = new Blob([csvContent], { type: "text/csv;charset=utf-8;" });
-
-    // Create a download link
-    const link = document.createElement("a");
-    link.href = window.URL.createObjectURL(blob);
-    link.download = "smartcard.csv";
-
-    // Trigger a click on the link to start the download
-    link.click();
+        // Trigger a click on the link to start the download
+        link.click();
+      })
+      .catch((error) => {
+        console.log("error in downloadBrandUploadTemplate:" + error);
+      });
   };
 
   const handleUploadFile = () => {
     if (selectedFiles.length === 0) {
+      console.log("No files selected to upload, handle accordingly");
       // No files selected, handle accordingly
       return;
     }
 
-    const file = selectedFiles[0];
-
-    const reader = new FileReader();
-    reader.onload = (e) => {
-      const csvContent = e.target.result;
-
-      // Process the CSV content (you can use a library like papaparse)
-      // For simplicity, let's just log the parsed data
-      const parsedData = parseCSV(csvContent);
-      console.log("Parsed CSV Data:", parsedData);
-    };
-
-    reader.readAsText(file);
-  };
-
-  const parseCSV = (csvContent) => {
-    // Use a CSV parsing library (e.g., papaparse)
-    // For simplicity, we'll split lines and split fields by commas
-    const lines = csvContent.split("\n");
-    const headers = lines[0].split(",");
-    const data = [];
-
-    for (let i = 1; i < lines.length; i++) {
-      const fields = lines[i].split(",");
-      const rowData = {};
-      for (let j = 0; j < headers.length; j++) {
-        rowData[headers[j]] = fields[j];
-      }
-      data.push(rowData);
+    if (!uploadTrigger || !uploadTrigger.token) {
+      console.log("No upload trigger found, handle accordingly");
+      // No upload trigger found, handle accordingly
+      return;
     }
+    const formData = new FormData();
+    formData.append("qFile", selectedFiles[0]); // appending file
 
-    // Move dispatch line above the return statement
-    // dispatch(onAddNewUser(data));
-    toggleUploadModal();
-    return data;
+    uploadSmartcardSubmit(uploadTrigger.token, formData)
+      .then((res) => {
+        // debugger;
+        toggleSuccessMsg();
+        console.log(
+          "res in uploadBrandFileForInitiatedUserUpload:" + JSON.stringify(res)
+        );
+
+        setUploadTrigger({});
+        setSelectedFiles([]);
+
+        console.log("cleared the selected files and upload trigger");
+        dispatch(onAddInventoryStockSmartcard(res.data.data));
+        toggle();
+      })
+      .catch((error) => {
+        console.log("error in upload:" + error);
+      });
   };
-
-  const validation = useFormik({
-    // enableReinitialize : use this flag when initial values needs to be changed
-    enableReinitialize: true,
-
-    initialValues: {
-      brand_id: "",
-      description: "",
-      inv_state_id: "",
-      invoice_date: "",
-      invoice_no: "",
-      is_embeded: false,
-      po_date: "",
-      po_number: "",
-      smartcardno: "",
-      state: "",
-      warehouse_id: "",
-      cas_id: "",
-      stbbrand_id: "",
-      stbno: "",
-      po_id: "",
-    },
-    validationSchema: Yup.object({
-      cas_id: Yup.string().required("Select CAS Type"),
-      brand_id: Yup.string().required("Select smartcard brand"),
-      smartcardno: Yup.string().required("Enter smartcard no."),
-      is_embeded: Yup.boolean(),
-      stbbrand_id: Yup.string().test(
-        "isRequired",
-        "Select stb brand",
-        function (value) {
-          const { is_embeded } = this.parent;
-          return is_embeded ? !!value : true;
-        }
-      ),
-      stbno: Yup.string().test("isRequired", "Enter STB no.", function (value) {
-        const { is_embeded } = this.parent;
-        return is_embeded ? !!value : true;
-      }),
-      // stbbrand_id: Yup.string().required("Select stb brand"),
-      // stbno: Yup.string().required("Enter STB no."),
-      po_number: Yup.string().required("Enter purchase order"),
-      po_date: Yup.string().required("Select purchase date"),
-      invoice_no: Yup.string().required("Enter invoice order"),
-      invoice_date: Yup.string().required("Select invoice date"),
-      warehouse_id: Yup.string().required("Select warehouse"),
-      state: Yup.string().required("Select Stock Type"),
-      description: Yup.string().required("description"),
-      inv_state_id: Yup.string().required("Select inentory state"),
-    }),
-    onSubmit: (values) => {
-      const newSmartcard = {
-        id: Math.floor(Math.random() * (30 - 20)) + 20,
-        cas_id: values["cas_id"],
-        is_embeded: values["is_embeded"],
-        brand_id: values["brand_id"],
-        smartcardno: values["smartcardno"],
-        stbbrand_id: values["stbbrand_id"],
-        stbno: values["stbno"],
-        po_number: values["po_number"],
-        po_date: values["po_date"],
-        invoice_no: values["invoice_no"],
-        invoice_date: values["invoice_date"],
-        warehouse_id: values["warehouse_id"],
-        state: values["state"],
-        description: values["description"],
-        inv_state_id: values["inv_state_id"],
-      };
-      console.log("New smartcard: " + JSON.stringify(newSmartcard));
-      //   dispatch(onAddInventoryStockSmartcard(newSmartcard));
-      //   dispatch(onGetInventoryStockSmartcard());
-      validation.resetForm();
-      toggle();
-    },
-    onReset: (values) => {
-      validation.setValues(validation.initialValues);
-    },
-  });
 
   return (
     <Modal
@@ -223,7 +177,7 @@ const UploadSmartcard = (props) => {
                 Download Sample Upload File
               </button>
             </div>
-            <Form>
+            {/* <Form>
               <Row>
                 <Col lg={4}>
                   <div className="mb-3">
@@ -646,7 +600,7 @@ const UploadSmartcard = (props) => {
                   </div>
                 </Col>
               </Row>
-            </Form>
+            </Form> */}
 
             <div className="text-center mt-4 ">
               <div
