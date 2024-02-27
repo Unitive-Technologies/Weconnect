b10a860e3af2c5cd24c732ed1667cc7f57cd8ecb
stella-mary <mariastellamary@outlook.com>
2024-02-27 10:54:11 +0530

    Updated  backend_helper

--- src/helpers/backend_helper.jsx
+++ src/helpers/backend_helper.jsx
@@ -460,8 +460,6 @@ export const uploadRegionalOfficeSubmit = async (initiatedToken, formData) => {
   );
 };
 
-<<<<<<< Updated upstream
-=======
 // export const downloadRegOffUploadTemplate = async (data) => {
 //   return await postCompleteResponse(url.DOWNLOAD_SAMPLE_REGIONALOFFICE, data);
 // };
@@ -478,7 +476,6 @@ export const uploadRegionalOfficeSubmit = async (initiatedToken, formData) => {
 // };
 
 
->>>>>>> Stashed changes
 // export const getDistrict = () => get(url.GET_DISTRICT);
 export const addDistrict = (district) => post(url.ADD_DISTRICT, district);
 export const getDistrictStateList = () => get(url.GET_DISTRICT_STATELIST);
