5cad26f349032c670a2d800189b189984d5c5559
stella-mary <mariastellamary@outlook.com>
2024-02-29 14:06:56 +0530

    Update UploadDistributorModal.jsx

--- src/pages/Organization/DistributorList/UploadDistributorModal.jsx
+++ src/pages/Organization/DistributorList/UploadDistributorModal.jsx
@@ -1,190 +1,3 @@
-// import React, { useEffect, useState, useRef, useMemo } from "react";
-// import PropTypes from "prop-types";
-// import {
-//   Card,
-//   CardBody,
-//   Col,
-//   Row,
-//   Modal,
-//   ModalHeader,
-//   ModalBody,
-//   Label,
-//   Input,
-//   Form,
-//   CardSubtitle,
-// } from "reactstrap";
-// import { Link } from "react-router-dom";
-// import Dropzone from "react-dropzone";
-
-// const UploadDistributorModal = (props) => {
-//   const { isOpen, handleUploadDistributor } = props;
-
-//   const [selectedFiles, setselectedFiles] = useState([]);
-
-//   function handleAcceptedFiles(files) {
-//     files.map((file) =>
-//       Object.assign(file, {
-//         preview: URL.createObjectURL(file),
-//         formattedSize: formatBytes(file.size),
-//       })
-//     );
-//     setselectedFiles(files);
-//   }
-
-//   function formatBytes(bytes, decimals = 2) {
-//     if (bytes === 0) return "0 Bytes";
-//     const k = 1024;
-//     const dm = decimals < 0 ? 0 : decimals;
-//     const sizes = ["Bytes", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"];
-
-//     const i = Math.floor(Math.log(bytes) / Math.log(k));
-//     return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + " " + sizes[i];
-//   }
-//   return (
-//     <Modal
-//       isOpen={isOpen}
-//       role="dialog"
-//       size="xl"
-//       autoFocus={true}
-//       centered={true}
-//       className="exampleModal"
-//       tabIndex="-1"
-//       toggle={handleUploadDistributor}
-//     >
-//       <ModalHeader toggle={handleUploadDistributor} tag="h4">
-//         Upload Distributor
-//       </ModalHeader>
-//       <ModalBody>
-//         <Card>
-//           <CardBody>
-//             <div className="text-left mb-4 r-0" style={{ marginLeft: "78%" }}>
-//               <button
-//                 type="button"
-//                 className="btn btn-primary"
-//                 // onClick={handleDownloadSampleFile}
-//               >
-//                 Download Sample Upload File
-//               </button>
-//             </div>
-//             <div className="mb-3">
-//               <Label className="form-label">Status</Label>
-//               <Input
-//                 name="status"
-//                 type="select"
-//                 placeholder="Select Group Policy"
-//                 className="form-select"
-//                 // onChange={validation.handleChange}
-//                 // onBlur={validation.handleBlur}
-//                 // value={validation.values.grouppolicy || ""}
-//               >
-//                 <option value="">Select Status</option>
-//                 <option value="A">Active</option>
-//                 <option value="C">In-Active</option>
-//               </Input>
-//               {/* {validation.touched.grouppolicy &&
-//               validation.errors.grouppolicy ? (
-//                 <FormFeedback type="invalid">
-//                   {validation.errors.grouppolicy}
-//                 </FormFeedback>
-//               ) : null} */}
-//             </div>
-//             <CardSubtitle className="mb-3"> Select File to Upload</CardSubtitle>
-//             <Form>
-//               <Dropzone
-//                 onDrop={(acceptedFiles) => {
-//                   handleAcceptedFiles(acceptedFiles);
-//                 }}
-//               >
-//                 {({ getRootProps, getInputProps }) => (
-//                   <div className="dropzone">
-//                     <div
-//                       className="dz-message needsclick mt-2"
-//                       {...getRootProps()}
-//                     >
-//                       <input {...getInputProps()} />
-//                       <div className="mb-3">
-//                         <i className="display-4 text-muted bx bxs-cloud-upload" />
-//                       </div>
-//                       <h4>Drop files here or click to upload.</h4>
-//                     </div>
-//                   </div>
-//                 )}
-//               </Dropzone>
-//               <div className="dropzone-previews mt-3" id="file-previews">
-//                 {selectedFiles.map((f, i) => {
-//                   return (
-//                     <Card
-//                       className="mt-1 mb-0 shadow-none border dz-processing dz-image-preview dz-success dz-complete"
-//                       key={i + "-file"}
-//                     >
-//                       <div className="p-2">
-//                         <Row className="align-items-center">
-//                           <Col className="col-auto">
-//                             <img
-//                               data-dz-thumbnail=""
-//                               height="80"
-//                               className="avatar-sm rounded bg-light"
-//                               alt={f.name}
-//                               src={f.preview}
-//                             />
-//                           </Col>
-//                           <Col>
-//                             <Link
-//                               to="#"
-//                               className="text-muted font-weight-bold"
-//                             >
-//                               {f.name}
-//                             </Link>
-//                             <p className="mb-0">
-//                               <strong>{f.formattedSize}</strong>
-//                             </p>
-//                           </Col>
-//                         </Row>
-//                       </div>
-//                     </Card>
-//                   );
-//                 })}
-//               </div>
-//             </Form>
-
-//             <div className="text-center mt-4 ">
-//               <div
-//                 style={{
-//                   display: "flex",
-//                   gap: 5,
-//                   textAlign: "center",
-//                   justifyContent: "center",
-//                 }}
-//               >
-//                 <button type="button" className="btn btn-primary mr-2 ">
-//                   Upload File
-//                 </button>
-//                 <button type="button" className="btn btn-primary ml-2 ">
-//                   Reset
-//                 </button>
-//                 <button
-//                   type="button"
-//                   className="btn btn-primary "
-//                   onClick={handleUploadDistributor}
-//                 >
-//                   Cancel
-//                 </button>
-//               </div>
-//             </div>
-//           </CardBody>
-//         </Card>
-//       </ModalBody>
-//       {/* </Modal> */}
-//     </Modal>
-//   );
-// };
-
-// UploadDistributorModal.propTypes = {
-//   toggle: PropTypes.func,
-//   isOpen: PropTypes.bool,
-// };
-
-// export default UploadDistributorModal;
 
 import React, { useState } from "react";
 import PropTypes from "prop-types";
@@ -221,7 +34,7 @@ const UploadDistributorModal = (props) => {
   const dispatch = useDispatch();
   const [uploadTrigger, setUploadTrigger] = useState({});
   const [selectedFiles, setSelectedFiles] = useState([]);
-  const [distributorsPhase, setDistributorsPhase] = useState("");
+  const [distributorPhase, setDistributorsPhase] = useState("");
   const [status, setStatus] = useState("");
   const [successMsg, setSuccessMsg] = useState(false);
 
@@ -232,7 +45,7 @@ const UploadDistributorModal = (props) => {
   function handleAcceptedFiles(files) {
     setSelectedFiles(files);
 
-    updateDistributorUploadByToken(uploadTrigger.token, brandSavedTemplatePayload)
+    updateDistributorUploadByToken(uploadTrigger.token, distributorSavedTemplatePayload)
       .then((res) => {
         console.log("res in updateDistributorUploadByToken:" + JSON.stringify(res));
       })
@@ -241,13 +54,13 @@ const UploadDistributorModal = (props) => {
       });
   }
 
-  const brandSavedTemplatePayload = {
-    meta_data: { type: 1, status: parseInt(status), state_id: parseInt(stateList) },
+  const distributorSavedTemplatePayload = {
+    meta_data: { type: 2, status: parseInt(status), reg_phase: parseInt(distributorPhase) },
     url: "",
   };
 
   const distributorDownloadTemplatePayload = {
-    meta_data: { type: 1, status: parseInt(status), state_id: parseInt(stateList) },
+    meta_data: { type: 2, status: parseInt(status), reg_phase: parseInt(distributorsPhase) },
     url: "",
   };
 
@@ -299,7 +112,7 @@ const UploadDistributorModal = (props) => {
         // debugger;
         toggleSuccessMsg();
         console.log(
-          "res in uploadBrandFileForInitiatedUserUpload:" + JSON.stringify(res)
+          "res in uploadDistributorFileForInitiatedUserUpload:" + JSON.stringify(res)
         );
 
         setUploadTrigger({});
@@ -365,14 +178,14 @@ const UploadDistributorModal = (props) => {
                   type="select"
                   placeholder="Select parent regional office"
                   className="form-select"
-                  value={stateList}
-                  onChange={(e) => setStateList(e.target.value)}
+                  value={distributorPhase}
+                  onChange={(e) => setDistributorsPhase(e.target.value)}
                 >
-                  <option value="">Select State</option>
+                  <option value="">Select </option>
                   {distributorsPhase &&
-                    distributorsPhase.map((state_id) => (
-                      <option key={state_id.id} value={state_id.id}>
-                        {state_id.name}
+                    distributorsPhase.map((reg_phase) => (
+                      <option key={reg_phase.id} value={reg_phase.id}>
+                        {reg_phase.name}
                       </option>
                     ))}
                 </Input>
@@ -388,8 +201,8 @@ const UploadDistributorModal = (props) => {
                   onChange={(e) => setStatus(e.target.value)}
                 >
                   <option value="">Select Status</option>
-                  {brandStatus &&
-                    brandStatus.map((status) => (
+                  {distributorsStatus &&
+                    distributorsStatus.map((status) => (
                       <option key={status.id} value={status.id}>
                         {status.name}
                       </option>
