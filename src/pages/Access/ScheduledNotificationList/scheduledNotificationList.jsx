37f8a6da61816bc1cc637c4c085296a967279daa
Anthoniselvi <anthoniselvi33@gmail.com>
2024-03-25 14:36:17 +0530

    Turn Active/InActive updated on ScheduledNotification

--- src/pages/Access/ScheduledNotificationList/scheduledNotificationList.jsx
+++ src/pages/Access/ScheduledNotificationList/scheduledNotificationList.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState, useMemo } from "react";
+import axios from "axios";
 import { Link } from "react-router-dom";
 import withRouter from "../../../components/Common/withRouter";
 import TableContainer from "../../../components/Common/TableContainer";
@@ -28,6 +29,47 @@ const ScheduledNotificationList = (props) => {
   document.title = "Scheduled Notification | VDigital";
 
   const dispatch = useDispatch();
+  const API_URL = "https://sms.unitch.in/api/index.php/v1";
+
+  const [isChecked, setIsChecked] = useState(true);
+  const [showBulkModal, setShowBulkModal] = useState(false);
+  const [showWarning, setShowWarning] = useState(false);
+  // const [selectedRows, setSelectedRows] = useState([]);
+  const [selectedRow, setSelectedRow] = useState({});
+  console.log(
+    "selectedRow in handleTurnOnActive: " + JSON.stringify(selectedRow)
+  );
+  const handleWarning = () => {
+    setShowWarning(!showWarning);
+  };
+
+  // const handleCheckboxClick = (row) => {
+  //   console.log(
+  //     "Before state update - selectedRows:",
+  //     JSON.stringify(selectedRows)
+  //   );
+  //   const isSelected = selectedRows.some(
+  //     (selectedRow) => selectedRow.id === row.id
+  //   );
+
+  //   if (isSelected) {
+  //     // If already selected, remove it
+  //     setSelectedRows((prevSelectedRows) =>
+  //       prevSelectedRows.filter((selectedRow) => selectedRow.id !== row.id)
+  //     );
+  //   } else {
+  //     // If not selected, add it
+  //     setSelectedRows((prevSelectedRows) => [...prevSelectedRows, row]);
+  //   }
+  //   console.log(
+  //     "After state update - selectedRows:",
+  //     JSON.stringify(selectedRows)
+  //   );
+  // };
+
+  const handleCheckboxClick = (row) => {
+    setSelectedRow(row);
+  };
 
   const selectScheduledNotificationState = (state) =>
     state.schedulednotification;
@@ -45,6 +87,21 @@ const ScheduledNotificationList = (props) => {
   // const [isLoading, setLoading] = useState(loading);
   const columns = useMemo(
     () => [
+      {
+        Header: "*",
+        disableFilters: true,
+        filterable: true,
+        disabled: true,
+        Cell: (cellProps) => (
+          <input
+            type="checkbox"
+            // checked={isChecked}
+            // onClick={() => handleCheckboxClick(cellProps.row.original)}
+            // disabled={!isChecked}
+            onChange={() => handleCheckboxClick(cellProps.row.original)}
+          />
+        ),
+      },
       {
         Header: "#",
         filterable: true,
@@ -237,6 +294,72 @@ const ScheduledNotificationList = (props) => {
 
   const keyField = "id";
 
+  const handleTurnOnActive = async () => {
+    try {
+      console.log(
+        "selectedRow in handleTurnOnActive: " + JSON.stringify(selectedRow)
+      );
+      const newAssign = {
+        id: selectedRow.id,
+        status: 1,
+      };
+      const token = "Bearer " + localStorage.getItem("temptoken");
+      const response = await axios.put(
+        `${API_URL}/schedule-announcement/${selectedRow.id}?vr=web1.0`,
+        newAssign,
+        {
+          headers: {
+            Authorization: token,
+          },
+        }
+      );
+
+      console.log("Axios Response:", response);
+      dispatch(onGetScheduledNotification());
+    } catch (error) {
+      console.error("Error in handleTurnOnActive:", error);
+    }
+  };
+
+  const handleTurnOnInActive = async () => {
+    try {
+      console.log(
+        "selectedRow in handleTurnOnActive: " + JSON.stringify(selectedRow)
+      );
+      const newAssign = {
+        id: selectedRow.id,
+        status: 0,
+      };
+      const token = "Bearer " + localStorage.getItem("temptoken");
+      const response = await axios.put(
+        `${API_URL}/schedule-announcement/${selectedRow.id}?vr=web1.0`,
+        newAssign,
+        {
+          headers: {
+            Authorization: token,
+          },
+        }
+      );
+
+      console.log("Axios Response:", response);
+      dispatch(onGetScheduledNotification());
+    } catch (error) {
+      console.error("Error in handleTurnOnActive:", error);
+    }
+  };
+
+  const getTableActions = () => {
+    return [
+      {
+        name: "Turn Active/InActive",
+        action:
+          selectedRow.status === 0 ? handleTurnOnActive : handleTurnOnInActive,
+        type: "normal",
+        // icon: "create",
+      },
+    ];
+  };
+
   return (
     <React.Fragment>
       <div className="page-content">
@@ -261,9 +384,8 @@ const ScheduledNotificationList = (props) => {
                       data={schedulednotify}
                       isGlobalFilter={true}
                       isShowingPageLength={true}
-                      // handleRowClick={(row) => {
-                      //   handleViewScheduledNotification(row);
-                      // }}
+                      isShowTableActionButtons={selectedRow && true}
+                      tableActions={getTableActions()}
                       // iscustomPageSizeOptions={true}
                       customPageSize={50}
                       tableClass="table align-middle table-nowrap table-hover"
